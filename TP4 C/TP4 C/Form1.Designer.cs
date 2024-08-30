3add0993557927872823c6715c43c149cbac726b
alpacino14 <94710092+alpacino14@users.noreply.github.com>
2024-08-30 17:54:32 -0300

    Add files via upload

---
+++ TP4 C/TP4 C/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace TP4_C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnprimos = new System.Windows.Forms.Button();
+            this.btnImpares = new System.Windows.Forms.Button();
+            this.btnpares = new System.Windows.Forms.Button();
+            this.lbnum = new System.Windows.Forms.ListBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.btngenerar = new System.Windows.Forms.Button();
+            this.txthasta = new System.Windows.Forms.TextBox();
+            this.txtdesde = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnprimos
+            // 
+            this.btnprimos.Location = new System.Drawing.Point(108, 288);
+            this.btnprimos.Name = "btnprimos";
+            this.btnprimos.Size = new System.Drawing.Size(110, 23);
+            this.btnprimos.TabIndex = 19;
+            this.btnprimos.Text = "Numeros Primos ";
+            this.btnprimos.UseVisualStyleBackColor = true;
+            this.btnprimos.Click += new System.EventHandler(this.btnprimos_Click);
+            // 
+            // btnImpares
+            // 
+            this.btnImpares.Location = new System.Drawing.Point(108, 235);
+            this.btnImpares.Name = "btnImpares";
+            this.btnImpares.Size = new System.Drawing.Size(110, 23);
+            this.btnImpares.TabIndex = 18;
+            this.btnImpares.Text = "Numeros Impares";
+            this.btnImpares.UseVisualStyleBackColor = true;
+            this.btnImpares.Click += new System.EventHandler(this.btnImpares_Click);
+            // 
+            // btnpares
+            // 
+            this.btnpares.Location = new System.Drawing.Point(108, 191);
+            this.btnpares.Name = "btnpares";
+            this.btnpares.Size = new System.Drawing.Size(110, 23);
+            this.btnpares.TabIndex = 17;
+            this.btnpares.Text = "Numeros Pares";
+            this.btnpares.UseVisualStyleBackColor = true;
+            this.btnpares.Click += new System.EventHandler(this.btnpares_Click);
+            // 
+            // lbnum
+            // 
+            this.lbnum.FormattingEnabled = true;
+            this.lbnum.Location = new System.Drawing.Point(297, 50);
+            this.lbnum.Name = "lbnum";
+            this.lbnum.Size = new System.Drawing.Size(192, 264);
+            this.lbnum.TabIndex = 16;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(341, 18);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(89, 13);
+            this.Label3.TabIndex = 15;
+            this.Label3.Text = "Lista de Numeros";
+            // 
+            // btngenerar
+            // 
+            this.btngenerar.Location = new System.Drawing.Point(108, 139);
+            this.btngenerar.Name = "btngenerar";
+            this.btngenerar.Size = new System.Drawing.Size(110, 23);
+            this.btngenerar.TabIndex = 14;
+            this.btngenerar.Text = "Generar Funcion";
+            this.btngenerar.UseVisualStyleBackColor = true;
+            this.btngenerar.Click += new System.EventHandler(this.btngenerar_Click);
+            // 
+            // txthasta
+            // 
+            this.txthasta.Location = new System.Drawing.Point(118, 87);
+            this.txthasta.Name = "txthasta";
+            this.txthasta.Size = new System.Drawing.Size(100, 20);
+            this.txthasta.TabIndex = 13;
+            // 
+            // txtdesde
+            // 
+            this.txtdesde.Location = new System.Drawing.Point(118, 50);
+            this.txtdesde.Name = "txtdesde";
+            this.txtdesde.Size = new System.Drawing.Size(100, 20);
+            this.txtdesde.TabIndex = 12;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(53, 95);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(35, 13);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "Hasta";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(50, 50);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(38, 13);
+            this.Label1.TabIndex = 10;
+            this.Label1.Text = "Desde";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 391);
+            this.Controls.Add(this.btnprimos);
+            this.Controls.Add(this.btnImpares);
+            this.Controls.Add(this.btnpares);
+            this.Controls.Add(this.lbnum);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.btngenerar);
+            this.Controls.Add(this.txthasta);
+            this.Controls.Add(this.txtdesde);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnprimos;
+        internal System.Windows.Forms.Button btnImpares;
+        internal System.Windows.Forms.Button btnpares;
+        internal System.Windows.Forms.ListBox lbnum;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Button btngenerar;
+        internal System.Windows.Forms.TextBox txthasta;
+        internal System.Windows.Forms.TextBox txtdesde;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+    }
+}
+
---
+++ TP4 C/TP4 C/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP4_C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngenerar_Click(object sender, EventArgs e)
+        {
+            lbnum.Items.Clear(); // Limpiar el ListBox antes de agregar numeros  
+
+            if (int.TryParse(txtdesde.Text, out int desde) && int.TryParse(txthasta.Text, out int hasta))
+            {
+                for (int i = desde; i <= hasta; i++)
+                {
+                    lbnum.Items.Add(i); // Agregar números al ListBox  
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese números válidos en ambos campos.");
+            }
+        }
+
+        private void btnpares_Click(object sender, EventArgs e)
+        {
+            lbnum.Items.Clear(); // Limpiar el ListBox  
+
+            // Validar campos  
+            if (!ValidarCampos(out int desde, out int hasta))
+                return;
+
+            // Agregar números pares al ListBox  
+            for (int i = desde; i <= hasta; i++)
+            {
+                if (i % 2 == 0) // Verificar si es par  
+                {
+                    lbnum.Items.Add(i.ToString());
+                }
+            }
+
+        } // Fin evento click boton pares
+
+
+
+        //Funcion para validar datos
+        private bool ValidarCampos(out int desde, out int hasta)
+        {
+            desde = 0;
+            hasta = 0;
+
+            if (string.IsNullOrWhiteSpace(txtdesde.Text) || string.IsNullOrWhiteSpace(txthasta.Text))
+            {
+                MessageBox.Show("Los campos desde y hasta no pueden estar vacíos. Ingrese valores.");
+                return false;
+            }
+
+            if (!int.TryParse(txtdesde.Text, out desde) || !int.TryParse(txthasta.Text, out hasta))
+            {
+                MessageBox.Show("Por favor, ingrese solo números válidos.");
+                return false;
+            }
+
+            if (desde > hasta)
+            {
+                MessageBox.Show("El valor desde no puede ser mayor que el valor Hasta");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnImpares_Click(object sender, EventArgs e)
+        {
+            lbnum.Items.Clear(); // Limpiar el ListBox  
+
+            // Validar campos  
+            if (!ValidarCampos(out int desde, out int hasta))
+                return;
+
+            // Agregar números impares al ListBox  
+            for (int i = desde; i <= hasta; i++)
+            {
+                if (i % 2 != 0) // Verificar si es impar  
+                {
+                    lbnum.Items.Add(i.ToString());
+                }
+            }
+
+        } // Fin evento click del boton Numero impares
+
+
+        //Metodo para saber si un num es primo recibe un num
+        private bool EsPrimo(int numero)
+        {
+            if (numero <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                    return false;
+            }
+            return true;
+        } // Fin metodo para detectar num primo
+
+        private void btnprimos_Click(object sender, EventArgs e)
+        {
+            lbnum.Items.Clear(); // Limpiar el ListBox  
+
+            // Validar campos  
+            if (!ValidarCampos(out int desde, out int hasta))
+                return;
+
+            // Agregar números primos al ListBox  
+            for (int i = desde; i <= hasta; i++)
+            {
+                if (EsPrimo(i))
+                {
+                    lbnum.Items.Add(i.ToString());
+                }
+            }
+
+        }
+    }
+}
